b44e9d76c9266cdc6b7493d5409af678e5292b76
VictoriaDrumeva <>
2021-10-28 11:56:46 +0000

    My first project

---
+++ Demos/W2_if.py
@@ -0,0 +1,8 @@
+first_number = int(input("Enter your number:"))
+second_number = int(input("Enter your second number:"))
+if(first_number > second_number):
+  print("First is bigger!")
+
+else: 
+  print("The second is bigger!")
+  
